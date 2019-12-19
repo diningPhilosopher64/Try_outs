b4a23e0eb09777962308df8e97346662ece09009
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-12-19 15:12:36 +0530

    code to get orgs selected for the past 2/3 years

---
+++ python/get_previous_orgs.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+last_two_years = ["https://summerofcode.withgoogle.com/archive/2019/organizations",
+                  "https://summerofcode.withgoogle.com/archive/2018/organizations"]
+
+last_three_years = ["https://summerofcode.withgoogle.com/archive/2019/organizations",
+                  "https://summerofcode.withgoogle.com/archive/2018/organizations",
+                   "https://summerofcode.withgoogle.com/archive/2017/organizations"]
+
+def get_previous_years_orgs(previous_years):
+    previous_years_sets = []
+    for year in previous_years:
+        content = requests.get(year)
+
+        if content.status_code == 200:
+            
+            soup = BeautifulSoup(content.text, 'html.parser')
+            targets = soup.findAll("h4", {"class":"organization-card__name"})
+
+            orgs = []
+            for target in targets:
+                orgs.append(target.text)
+
+            this_set = set(orgs)
+            previous_years_sets.append(this_set)
+            
+            first_set = previous_years_sets[0]
+            
+            iterator= 1
+            
+            while iterator < len(previous_years_sets):
+                first_set = first_set.intersection(previous_years_sets[iterator])
+                iterator += 1
+                
+    return list(first_set)
+            
+
+
+list1 = get_previous_years_orgs(["https://summerofcode.withgoogle.com/archive/2019/organizations"])
+
+list2 = get_previous_years_orgs(["https://summerofcode.withgoogle.com/archive/2018/organizations"])
+
+test1 = list(set(list1).intersection(set(list2)))
+
+test2 = get_previous_years_orgs(last_two_years)
+
+test1.sort()
+test2.sort()
+if test1 == test2:
+    print("Yes")
+else:
+    print("No")
